32848ad1fa3e56641c36218f1569877e4d7581d0
Ralph Quirequire <ralph.quirequire@outlook.com>
2016-06-08 14:39:36 +0800

    Enabled debug mode

--- app/app.py
+++ app/app.py
@@ -28,5 +28,6 @@ def index():
                             choices=LOTTODRAWS)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(debug=True)
+    #app.run(host='0.0.0.0')
     
