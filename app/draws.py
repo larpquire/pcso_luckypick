e2bc045b9873f02c01d2fbd983a3ac6f97e72b26
Ralph Quirequire <ralph.quirequire@outlook.com>
2016-06-09 10:22:44 +0800

    Cleaned up code

--- app/draws.py
+++ app/draws.py
@@ -3,7 +3,8 @@ from collections import OrderedDict
 import numpy as np
 
 # globals
-LOTTODRAWS = {  '2_1_31_repl':'EZ 2 Lotto',
+LOTTODRAWS = {  
+                '2_1_31_repl':'EZ 2 Lotto',
                 '3_0_9_repl':'SwerTres Lotto',
                 '4_0_9_repl':'4-digit Lotto',
                 '6_0_9_repl':'6-digit Lotto',
@@ -11,7 +12,8 @@ LOTTODRAWS = {  '2_1_31_repl':'EZ 2 Lotto',
                 '6_1_45_nrep':'Mega Lotto 6/45',
                 '6_1_49_nrep':'Super Lotto 6/49',
                 '6_1_55_nrep':'Grand Lotto 6/55',
-                '6_1_58_nrep':'Ultra Lotto 6/58'  }
+                '6_1_58_nrep':'Ultra Lotto 6/58'  
+            }
 
 LOTTODRAWS = OrderedDict(sorted(LOTTODRAWS.items(), key=lambda(k, v):k))
 #keyval = sorted(LOTTODRAWS.items(), key=lambda(k, v):k)
@@ -30,12 +32,22 @@ class LottoGame(object):
     def parse_args(self):
         digits, low, up, samp = self.draw.split('_')
         
-        return {'digits':int(digits), 'low':int(low),
-                'up':int(up), 'samp':samp}
+        return {
+                    'digits':int(digits),
+                    'low':int(low),
+                    'up':int(up),
+                    'samp':samp
+                }
+
     
     def generate_result(self):
         repl = {'repl':True, 'nrep':False}
         
-        return np.random.choice(xrange(self.args['low'], self.args['up']+1),
-                                size=self.args['digits'],
-                                replace=repl[self.args['samp']])
+        return np.random.choice(
+                                    xrange(
+                                                self.args['low'],
+                                                self.args['up']+1
+                                            ),
+                                    size=self.args['digits'],
+                                    replace=repl[self.args['samp']]
+                                )
